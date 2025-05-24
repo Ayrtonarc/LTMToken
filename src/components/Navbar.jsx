92ebd1206f8fbeeefaa639f7c85a5a57406dae11
Ayrton Santos <ayrton.arc@gmail.com>
2025-05-23 22:46:17 -0700

    upgrades

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,15 @@ import "./Navbar.css";
 
 const Navbar = () => (
   <nav className="navbar">
-    <div className="navbar-container">
-      <a href="#hero" className="navbar-logo">LTM Crypto</a>
-      <ul className="navbar-links">
-        <li><a href="#about">¿Qué es?</a></li>
-        <li><a href="#features">Ventajas</a></li>
-        <li><a href="#whitepaper">Whitepaper</a></li>
-        <li><a href="#coming-soon">Próximamente</a></li>
-      </ul>
+    <div className="container">
+      <a href="/" className="navbar-logo">LTM Crypto</a>
+      <div className="navbar-links">
+        <a href="#about">¿Qué es?</a>
+        <a href="#features">Ventajas</a>
+        <a href="#whitepaper">Whitepaper</a>
+        <a href="#faq">FAQ</a>
+        <a href="#contact">Contacto</a>
+      </div>
     </div>
   </nav>
 );
